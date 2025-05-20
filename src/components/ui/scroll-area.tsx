cf2d1714ef61ac6d262cca4f394e953380ea1793
Jose Martin Quesada <josemartinquesada@gmail.com>
2025-05-20 09:22:42 +0100

    DEBUG: Set scroll-area thumb to red for visibility test

--- src/components/ui/scroll-area.tsx
+++ src/components/ui/scroll-area.tsx
@@ -39,7 +39,7 @@ const ScrollBar = React.forwardRef<
     )}
     {...props}
   >
-    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-none rounded-full bg-[hsl(var(--foreground)/0.5)]" />
+    <ScrollAreaPrimitive.ScrollAreaThumb className="relative flex-none rounded-full bg-red-500" />
   </ScrollAreaPrimitive.ScrollAreaScrollbar>
 ))
 ScrollBar.displayName = ScrollAreaPrimitive.ScrollAreaScrollbar.displayName
