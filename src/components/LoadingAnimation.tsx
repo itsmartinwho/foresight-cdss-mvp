7557093c35cd55ce56c1c89716e7c76877c61693
Jose Martin Quesada <josemartinquesada@gmail.com>
2025-05-21 12:38:34 +0100

    new loading animation

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LoadingAnimation = () => {
   return (
     <div className="flex items-center justify-center min-h-[calc(100vh-4rem)] w-full">
-      <img src="/load-animation-small-quick.gif" alt="Loading..." />
+      <img src="/slower-load-animation.gif" alt="Loading..." />
     </div>
   );
 };
