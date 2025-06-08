75a51001aabc74a7444bf6df82938f056aed5da3
Jose Martin Quesada <josemartinquesada@gmail.com>
2025-06-08 17:11:14 +0100

    Implement scrollable differential diagnoses container - Restructure DifferentialDiagnosesList to use flex layout with fixed header - Add proper overflow-y-auto scroll container with min-h-0 for flexbox - Remove wrapper div blocking height constraints

--- src/components/modals/ConsultationPanel.tsx
+++ src/components/modals/ConsultationPanel.tsx
@@ -830,14 +830,12 @@ export default function ConsultationPanel({
                       </div>
                     )}
                     {planGenerated && activeTab === 'differentials' && (
-                      <div className="h-full flex flex-col space-y-4">
-                        <DifferentialDiagnosesList
-                          diagnoses={differentialDiagnoses}
-                          isLoading={isLoadingDifferentials}
-                          isEditable={!isDemoMode}
-                          className="flex-1 overflow-y-auto"
-                        />
-                      </div>
+                      <DifferentialDiagnosesList
+                        diagnoses={differentialDiagnoses}
+                        isLoading={isLoadingDifferentials}
+                        isEditable={!isDemoMode}
+                        className="h-full"
+                      />
                     )}
                     {planGenerated && activeTab === 'diagnosis' && (
                       <div className="h-full flex flex-col space-y-4">
